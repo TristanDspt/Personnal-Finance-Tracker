0b812bc8a0069091f8b528ec63e156161a5d8ebe
TristanDspt <tristan.despont@hotmail.fr>
2026-02-18 17:48:00 +0100

    maj archi globale du projet

--- run/run_bourse.bat
+++
@@ -1,8 +0,0 @@
-:: Lance le script
-
-@echo off
-:: On remonte d'un dossier pour être à la racine du projet
-cd /d "%~dp0.."
-
-:: On lance avec ton Venv (Chemin absolu pour ton PC)
-"E:\Venvs\pft_env\scripts\python.exe" "scripts\update_price.py"
